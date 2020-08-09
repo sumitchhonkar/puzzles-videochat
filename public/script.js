33f12d3f68aeffd93c258a019c7507477b9a4c41
Chhonkar, Sumit <SChhonkar@cvent.com>
2020-08-09 18:51:42 +0530

    Added mute and play buttons

--- public/script.js
+++ public/script.js
@@ -4,6 +4,11 @@ const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
+const scrollToBottom = () => {
+    var d = $('.main_chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
@@ -63,4 +68,61 @@ $('html').keydown((e) => {
 
 socket.on('createMessage', message => {
     $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+    scrollToBottom();
 }) 
+
+// Mute or Unmute audio
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+  }
+
+  const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main_mute_button').innerHTML = html;
+  }
+  
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main_mute_button').innerHTML = html;
+  }
+
+  //Play or stop video
+  const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo()
+    } else {
+      setStopVideo()
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+  }
+
+  const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document.querySelector('.main_video_button').innerHTML = html;
+  }
+  
+  const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>
+    `
+    document.querySelector('.main_video_button').innerHTML = html;
+  }
