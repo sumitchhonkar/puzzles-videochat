5fc2b48b6f3bde92d49b975a6aaec16cd72eb836
Chhonkar, Sumit <SChhonkar@cvent.com>
2020-08-09 16:45:51 +0530

    Added call answer

--- public/script.js
+++ public/script.js
@@ -26,16 +26,28 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream) //We answer the other's person call
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => { //And add the video stream here
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+    })
 })
 
 peer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
 })
 
-socket.on('user-connected', (userId) => {
-    connectToNewUser(userId);
-})
-
-const connectToNewUser = (userId) => {
-    console.log(userId);
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream) //Other user calls us
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
 }
