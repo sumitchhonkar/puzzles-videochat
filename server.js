c065d0366d6da8768f855baffd8fdbe0c8aaae3b
Chhonkar, Sumit <SChhonkar@cvent.com>
2020-08-09 18:55:42 +0530

    Removed comments

--- server.js
+++ server.js
@@ -13,14 +13,12 @@ const peerServer = ExpressPeerServer(server, {
   debug: true
 });
 
-app.set('view engine', 'ejs'); //ejs is used to transfer variables from backend(node.js) to our front end
+app.set('view engine', 'ejs'); 
 app.use(express.static('public'));
 
 app.use('/peerjs', peerServer);
 
 app.get('/', (req,res) => {
-   // res.status(200).send("Hello World!");
-   //res.render('room');
    res.redirect(`/${uuidv4()}`);
 });
 
@@ -30,7 +28,6 @@ app.get('/:room', (req,res) => {
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-       // console.log("joined room");
        socket.join(roomId);
        socket.to(roomId).broadcast.emit('user-connected', userId);
        socket.on('message', message => {
@@ -39,5 +36,4 @@ io.on('connection', socket => {
 
     })
 })
-
 server.listen(3030);
